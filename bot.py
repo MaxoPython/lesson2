8cdca1037a8d810ee5204c9b5b1aed311425b28b
Maxim Odintsov <maxim.odintsov@inwin.tech>
2019-03-02 11:33:17 +0300

    Рабочий бот

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters #добавление модуля и обработчиков телеграм
 
-import logging                                                            #добавление  обработчиков python
+import datetime
+import ephem
+import logging   
+                                                         #добавление  обработчиков python
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
@@ -19,14 +22,29 @@ def talk_to_me(bot,update):
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.last_name, update.message.chat.id, 
                  update.message.text)
     update.message.reply_text(user_text)
-       
+
+def planets(bot,update):
+    c_time=datetime.datetime.now()                             #привести время в нужный формат!
+    current_time=c_time.strftime('%d.%m.%Y')
+
+    input_date = update.message.text
+    planet_name=input_date.split()
+    if planet_name[1]=='mars':
+        mars=ephem.Mars(current_time)
+        planet_text=ephem.constellation(mars)
+        update.message.reply_text(planet_text)
+    else:
+        update.message.reply_text('Не та планета')
+
 def main():
+
     mybot = Updater("785187042:AAGuh5KxPuU75flk04CfrZmFHubWuyqkUL8",request_kwargs=PROXY)
     
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start",greet_user))
+    dp.add_handler(CommandHandler('planet',planets))
     dp.add_handler(MessageHandler(Filters.text,talk_to_me))
 
     mybot.start_polling() #сходи и получи новые данные
---
+++ planets.py
@@ -0,0 +1,2 @@
+import ephem
+print(ephem.next_full_moon('2016/09/23'))
